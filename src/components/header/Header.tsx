f816be680012d328055168b09f140c2c1844fbbd
Aleksey Pnev <pnev.leha@mail.ru>
2024-04-14 14:19:00 +0700

    fix package json | bugs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,17 +4,17 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
     const isAuth = true;
-    const isStudent = true;
+    const isStudent = false;
 
     return (
         <header className={isAuth ? styles.headerAuth : styles.header}>
             <Logo />
             {!isAuth ? (
                 <div className={styles.notAuth}>
-                    <Link to={"/registration"}>
+                    <Link to={"/registration"} className={styles.button1}>
                         Регистрация
                     </Link>
-                    <Link to={"/login"}>
+                    <Link to={"/login"} className={styles.button2}>
                        Вход
                     </Link>
                 </div>
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -4,11 +4,9 @@ import styles from "./Logo.module.scss";
 
 export default function Logo(){
     return(
-        <Link to={"/"}>  
-            <div className={styles.logo}>
-                <img src={logo} alt="logo" />
-                <div>SFU-Карьера</div>
-            </div>
+        <Link to={"/"} className={styles.logo}>  
+            <img src={logo} alt="logo" />
+            <div>SFU-Карьера</div>
         </Link>
     )
 }
